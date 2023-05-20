158356cd476c1ce4d883e09957876a8edccee35a
Ahmed <beezer@beezsoft.com>
2023-05-21 01:28:04 +0300

    hotfix

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import EventService from '../services/EventService'
+import { onMounted, ref } from 'vue'
+
+const eventDetail = ref(null)
+const id = ref(123)
+
+onMounted(() => {
+  EventService.getEventDetail(id)
+    .then((response) => {
+      eventDetail.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+</script>
+
+<template>
+  <div class="event-card">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
